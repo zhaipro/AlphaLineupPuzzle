f7023a4de882e59e3e1d255e044b302a83d69fd5
zhai_pro <xin_soft@foxmail.com>
2016-04-14 11:58:12 +0800

    深度优先搜索一颗固定深度的树
    选择能最大化置信区间上界的分支

--- AlphaLineupPuzzle/mcts.py
+++ AlphaLineupPuzzle/mcts.py
@@ -1,32 +1,121 @@
 # coding: utf-8
+import random
+import math
+
+from AlphaLineupPuzzle import lineup_puzzle
 
 
-def select(gs, deep):
-    optimal_move = None
-    max_score = gs.score
+class StateNode(object):
 
-    for idx, pos in gs.legal_moves():
-        _gs = gs.copy()
-        _gs.move(idx, pos)
-        score = ext(_gs, idx, deep - 1)
-        if score >= max_score:
-            max_score = score
-            optimal_move = (idx, pos)
+    # 这将会是一颗巨大的搜索树
+    # 以下语句可以节省大量内存
+    __slots__ = ('gs', 'value', 'children', 'depth')
 
-    return optimal_move, max_score
+    def __init__(self, gs, depth):
+        self.gs = gs
+        self.value = 0
+        self.children = None
+        self.depth = depth
+
+    def select(self):
+        for child in self.children:
+            if child.n < 2:
+                return child
+
+        max_child = None
+        max_value = 0
+        for child in self.children:
+            dalta = (child.value - child.gs.score)
+
+            a = child.value + dalta / math.sqrt(child.n)
+            if a > max_value:
+                max_value = a
+                max_child = child
+        return max_child
+
+    def simulate(self):
+        return self.gs.score
+
+    def visit(self):
+        if self.depth == 0:
+            value = self.simulate()
+        else:
+            if self.children is None:
+                self.ext()
+            child = self.select()
+            if child:
+                child.visit()
+                value = child.value
+            else:   # Game Over
+                value = self.gs.score
+        value = float(value)
+        self.update(value)
+
+    def is_leaf(self):
+        return self.depth == 0
+
+    def update(self, value):
+        if self.children:
+            child = max(self.children)
+            self.value = child.value
+        else:
+            self.value = value
+
+    def ext(self):
+        self.children = []
+        for action in self.gs.legal_moves():
+            gs = self.gs.copy()
+            an = ActionNode(gs, action, self.depth)
+            self.children.append(an)
+
+    @staticmethod
+    def get_move(gs, depth, times):
+        root = StateNode(gs, depth)
+
+        for _ in xrange(times):
+            root.visit()
+
+        if root.children:
+            child = max(root.children)
+            return child.action, child.value
+        else:
+            return None, 0
 
 
-def ext(gs, idx, deep):
-    if deep == 0:
-        return gs.score
+class ActionNode(StateNode):
 
-    count = 0
-    sum_score = 0
-    for _gs in gs.ext(idx):     # 扩展
-        count += 1
-        _, score = select(_gs, deep)
-        sum_score += score
-    avg_score = float(sum_score) / count
-    return avg_score
+    __slots__ = ('n', 'action')
 
-search = select
+    def __init__(self, gs, action, depth):
+        super(ActionNode, self).__init__(gs, depth)
+        self.n = 0
+        self.action = action
+        self.gs = gs.copy()
+        self.gs.move(*action)
+
+    def __lt__(self, other):
+        return self.value < other.value
+
+    def update(self, value):
+        self.value = (self.n * self.value + value) / (self.n + 1)
+        self.n += 1
+
+    def select(self):
+        return random.choice(self.children)
+
+    def ext(self):
+        self.children = []
+        for gs in self.gs.ext(self.action[0]):
+            gs = gs.copy()
+            sn = StateNode(gs, self.depth - 1)
+            self.children.append(sn)
+
+lineup_puzzle.Block.init()
+gs = lineup_puzzle.GameState.create()
+while True:
+    print gs
+    action, value = StateNode.get_move(gs, 5, 6000)
+    if action is None:
+        break
+    gs.move(*action)
+    print action, value, gs.score, value - gs.score
