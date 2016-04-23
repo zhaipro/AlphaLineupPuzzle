5029cf37c142c6ab3f82dbc75743d6e761f65d07
zhai_pro <xin_soft@foxmail.com>
2016-04-23 13:27:40 +0800

    完善转换程序

--- AlphaLineupPuzzle/preprocessing/game_converter.py
+++ AlphaLineupPuzzle/preprocessing/game_converter.py
@@ -3,8 +3,6 @@ import os
 import argparse
 import json
 
-import h5py as h5
-
 from AlphaLineupPuzzle import lineup_puzzle
 from AlphaLineupPuzzle.preprocessing import state_to_tensor
 from AlphaLineupPuzzle.preprocessing import action_to_tensor
@@ -34,6 +32,7 @@ def convert_game(in_fn):
 
 
 def to_hdf5(sgf_files, hdf5_file):
+    import h5py as h5
     # make a hidden temporary file in case of a crash.
     # on success, this is renamed to hdf5_file
     dirname = os.path.dirname(hdf5_file)
@@ -42,7 +41,7 @@ def to_hdf5(sgf_files, hdf5_file):
     h5f = h5.File(tmp_file, 'w')
 
     count = 0
-    for fn in sgf_files:
+    for fn in sgf_files:`
         for state, action in convert_game(fn):
             h5f.create_dataset('state_%d' % count, data=state, compression='lzf')
             h5f.create_dataset('action_%d' % count, data=action, compression='lzf')
@@ -55,6 +54,10 @@ def to_hdf5(sgf_files, hdf5_file):
 if __name__ == '__main__':
 
     parser = argparse.ArgumentParser(description='#todo')
-    parser.add_argument("infolder", help="游戏存档目录")
-    parser.add_argument("outfolder", help="输出目录")
+    parser.add_argument('infolder', help='游戏存档目录')
+    parser.add_argument('-o', default='a.hdf5', help='输出目录')
+    parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
+    fns = os.listdir(args.infolder)
+    fns = (os.path.join(args.infolder, fn) for fn in fns)
+    to_hdf5(fns, args.o)
