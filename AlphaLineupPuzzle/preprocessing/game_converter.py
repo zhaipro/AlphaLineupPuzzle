49a32ddc338e78fdf6d0881373870c04fa0141a0
zhai_pro <xin_soft@foxmail.com>
2016-04-24 13:12:36 +0800

    变更在hdf5中的存储方式

--- AlphaLineupPuzzle/preprocessing/game_converter.py
+++ AlphaLineupPuzzle/preprocessing/game_converter.py
@@ -3,6 +3,8 @@ import os
 import argparse
 import json
 
+import numpy as np
+
 from AlphaLineupPuzzle import lineup_puzzle
 from AlphaLineupPuzzle.preprocessing import state_to_tensor
 from AlphaLineupPuzzle.preprocessing import action_to_tensor
@@ -31,7 +33,20 @@ def convert_game(in_fn):
         gs.move(*action, next_alternative=next_alternative)
 
 
+def create_dataset_like(h5f, name, data):
+    # 创建一个数据集
+    dataset = h5f.create_dataset(name,
+                                 dtype=np.float32,                  # chainer要求数据必须是32为浮点型
+                                 shape=(1,) + data.shape,
+                                 maxshape=(None,) + data.shape,     # 'None' dimension allows it to grow arbitrarily
+                                 chunks=(64,) + data.shape,
+                                 compression='lzf')
+    return dataset
+
+
+
 def to_hdf5(sgf_files, hdf5_file):
+    # http://docs.h5py.org/en/latest/high/group.html#Group.create_dataset
     import h5py as h5
     # make a hidden temporary file in case of a crash.
     # on success, this is renamed to hdf5_file
@@ -41,15 +56,26 @@ def to_hdf5(sgf_files, hdf5_file):
     h5f = h5.File(tmp_file, 'w')
 
     count = 0
-    for fn in sgf_files:`
+    for fn in sgf_files:
         for state, action in convert_game(fn):
-            h5f.create_dataset('state_%d' % count, data=state, compression='lzf')
-            h5f.create_dataset('action_%d' % count, data=action, compression='lzf')
+            if count:
+                states.resize((count + 1,) + state.shape)
+                actions.resize((count + 1,) + action.shape)
+            else:
+                states = create_dataset_like(h5f, 'states', state)
+                actions = create_dataset_like(h5f, 'actions', action)
+            states[count] = state
+            actions[count] = action
             count += 1
 
     h5f.close()
-
     os.rename(tmp_file, hdf5_file)
+
+
+def from_hdf5(hdf5_file):
+    import h5py as h5
+    h5f = h5.File(hdf5_file)
+    return h5f['states'].value, h5f['actions'].value
 
 if __name__ == '__main__':
 
@@ -59,5 +85,5 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--verbose', action='store_true')
     args = parser.parse_args()
     fns = os.listdir(args.infolder)
-    fns = (os.path.join(args.infolder, fn) for fn in fns)
+    fns = (os.path.join(args.infolder, fn) for fn in fns if fn.endswith('.json'))
     to_hdf5(fns, args.o)
