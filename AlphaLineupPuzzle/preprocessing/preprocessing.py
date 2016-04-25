5f31c1f24e3262130d9a058c2c72fa188d9e526d
zhai_pro <xin_soft@foxmail.com>
2016-04-25 18:01:44 +0800

    增加特征

--- AlphaLineupPuzzle/preprocessing/preprocessing.py
+++ AlphaLineupPuzzle/preprocessing/preprocessing.py
@@ -11,12 +11,18 @@ def int_to_onehot(digit, max_value):
 
 
 def state_to_tensor(gs):
+    # 候选项
     blocks = len(lineup_puzzle.Block.blocks)
     feat_tensors = [int_to_onehot(idx, blocks) for idx in gs.alternative]
+    # 棋盘当前位置是否有子
     feat_tensors.append(gs.board.reshape(gs.board.size))
+    # 棋盘当前位置可放置哪些候选项
+    f = np.zeros((3, gs.size, gs.size))
+    for idx, (x, y) in gs.legal_moves():
+        f[idx, x, y] = 1
+    feat_tensors.append(f.reshape(f.size))
     return np.concatenate(feat_tensors)
 
 
 def action_to_tensor(idx, (x, y), size=7):
-    oh = np.zeros((size, size, 3))
     return np.array((x * 7 + y) * 3 + idx)
